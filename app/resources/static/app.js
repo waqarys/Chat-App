a4a0d80552e28d589256e1aff8d77f1bc804d04c
waqarys <waqaryshaikh@gmail.com>
2021-11-06 16:04:54 +0530

    database-backed chat app

--- app/resources/static/app.js
+++ app/resources/static/app.js
@@ -1,14 +1,15 @@
 function submitForm() {
   fetch(
     "/",
-    {method: "POST", body: JSON.stringify({name: nameInput.value, msg: msgInput.value})}
+    {method: "POST", body: JSON.stringify({
+        parent: parentInput.value, name: nameInput.value, msg: msgInput.value
+    })}
   ).then(response => response.json())
    .then(json => {
-     if (json["success"]) msgInput.value = ""
-     errorDiv.innerText = json["err"]
-   })
+    if (json["success"]) msgInput.value = ""
+    errorDiv.innerText = json["err"]
+  })
 }
 
-var socket = new WebSocket("ws://" + location.host + "/subscribe")
-socket.onopen = function (ev) { socket.send(filterString) };
-socket.onmessage = function(ev) { messageList.innerHTML = ev.data }
+var socket = new WebSocket("ws://" + location.host + "/subscribe");
+socket.onmessage = function (ev) { messageList.innerHTML = ev.data }
